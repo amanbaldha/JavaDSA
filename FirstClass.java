a0e6a4ef29967f9d33258e6c2b2a4be143fd2b84
Aman Baldha <169175825+amanbaldha@users.noreply.github.com>
2024-07-29 22:04:34 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class FirstClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int name = a + b;
+        System.out.println(name);
+    }
+}
