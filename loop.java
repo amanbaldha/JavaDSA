10d8314f163f3bdd264fbcf0d4b975593205b7e8
Aman Baldha <169175825+amanbaldha@users.noreply.github.com>
2024-07-30 18:13:40 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,32 @@
+public class loop {
+
+    public static void main(String[] args) {
+        // for(int i=0; i<11; i++){
+            // System.out.println(i);
+        // }
+
+
+        // int i=0;
+        // while(i<11){
+            // System.out.println(i);
+            // i++;
+        // }
+
+        // int i=0;
+        // do{
+        //     System.out.println(i);
+        //     i++; 
+        // }while(i<11);
+
+
+        // int i = 12;
+        // while (i<11) {
+        //     System.out.println("aman");
+        // }
+
+        int i=12;
+        do{
+            System.out.println("aman");
+        }while(i<11);
+    }
+}
